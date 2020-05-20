a4799f507f8a31f849c52a19e4f4b76c8fab2be4
Abtahee Ali <abtaheeali@gmail.com>
2020-05-20 17:08:33 -0400

    Added choices for user

--- Dinosaurs.cs
+++ Dinosaurs.cs
@@ -2,21 +2,22 @@ using System;
 
 namespace JurassicPark
 {
-    class Dinosaurs
+    class Dinosaur
     {
         public string Name { get; set; }
 
         public string DietType { get; set; }
 
-        public string WhenAcquired { get; set; }
-        // figure out if whenAcquired is a string int, etc
+        public DateTime WhenAcquired { get; set; }
+
         public int Weight { get; set; }
 
         public int EnclosedNumber { get; set; }
 
         public string Description()
         {
-            var descriptionOfDinosaurs = ($"{Name} was acquired to us around {WhenAcquired} and weighs in about {Weight} lbs. You can locate {Name} in  "
+            var descriptionOfDinosaurs = ($"{Name} was acquired to us around {WhenAcquired} and is located in pin {EnclosedNumber}. {Name} weighs in about {Weight} and eats {DietType}");
+            return descriptionOfDinosaurs;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JurassicPark
 {
@@ -33,16 +34,91 @@ namespace JurassicPark
         {
             //          | Name    | dietType | weight | WhenAcquired | EnclosureNumber | 
             // | ------- | -------- | ------ | ------------ | --------------- | 
-            // | Ally    |carnivore | 1 ton  |              | 1               |
-            // | Bob     |herbivore | 3 ton  |              | 2               | 
-            // | Rock    |carnivore | 10 ton |              | 3               | 
-            // | Apple   |carnivore  | 5 ton |              | 4               | 
-            var Ally = new Dinosaurs
+            // | Ally    |carnivore | 2000  |              | 1               |
+            // | Bob     |herbivore | 3000  |              | 2               | 
+            // | Rock    |carnivore | 10000 |              | 3               | 
+            // | Apple   |carnivore  | 5000 |              | 4               | 
+
+            var Ally = new Dinosaur
             {
                 Name = "Ally",
                 DietType = "Carnivore",
-                Weight = "1 ton",
+                Weight = 1000,
+                WhenAcquired = DateTime.Now,
+                EnclosedNumber = 1,
+            };
+
+            var Bob = new Dinosaur
+            {
+                Name = "Bob",
+                DietType = "Herbivore",
+                Weight = 3000,
+                WhenAcquired = "COMEBACK",
+                EnclosedNumber = 2,
+            };
+
+            var Rock = new Dinosaur
+            {
+                Name = "Rock",
+                DietType = "Carnivore",
+                Weight = 10000,
+                WhenAcquired = "COMEBACK TO THIS",
+                EnclosedNumber = 3,
+            };
+
+            var Apple = new Dinosaur
+
+            {
+                Name = "Apple",
+                DietType = "Carnivore",
+                Weight = 5000,
+                WhenAcquired = ,
+                EnclosedNumber = 4,
+            };
+
+            var listOfDinosaurs = new List<Dinosaur>();
+            listOfDinosaurs.Add(Ally);
+            listOfDinosaurs.Add(Bob);
+            listOfDinosaurs.Add(Rock);
+            listOfDinosaurs.Add(Apple);
+
+            Console.WriteLine();
+            Console.WriteLine("Welcome to the zoo of Dinosaurs");
+            Console.WriteLine();
+
+
+            var userHasQuitApp = false;
+
+            while (userHasQuitApp == false) ;
+
+            {
+                Console.WriteLine("Please select an option from the following choices:");
+
+                Console.WriteLine("(V)iew the list of Dinosaurs that are currently as the zoo!");
+
+                Console.WriteLine("(A)dd a new Dinosaur to our list!");
+
+                Console.WriteLine("(R)emove a Dinosaur from our list.");
+
+                Console.WriteLine("(T)ransfer a Dinosaur to a new pin.");
+
+                Console.WriteLine("(S)ummary of the Dinosaurs diet.");
+
+                Console.WriteLine("(Q)uit the Application.");
+
+
+                var choice = PromptForString("Choice: ");
+
+                if (choice == "V")
+                {
+
+                }
             }
+
+
+
+
+
         }
     }
 }
