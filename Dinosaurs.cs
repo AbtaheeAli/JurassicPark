c22eee732a4b70b15ad309cf24152c57085ee1a3
Abtahee Ali <abtaheeali@gmail.com>
2020-05-20 15:12:28 -0400

    Created var for Dinosaurs and stored Dinosaurs in a list

--- Dinosaurs.cs
+++ Dinosaurs.cs
@@ -4,6 +4,19 @@ namespace JurassicPark
 {
     class Dinosaurs
     {
+        public string Name { get; set; }
 
+        public string DietType { get; set; }
+
+        public string WhenAcquired { get; set; }
+        // figure out if whenAcquired is a string int, etc
+        public int Weight { get; set; }
+
+        public int EnclosedNumber { get; set; }
+
+        public string Description()
+        {
+            var descriptionOfDinosaurs = ($"{Name} was acquired to us around {WhenAcquired} and weighs in about {Weight} lbs. You can locate {Name} in  "
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,9 +5,44 @@ namespace JurassicPark
 
     class Program
     {
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C#");
+            //          | Name    | dietType | weight | WhenAcquired | EnclosureNumber | 
+            // | ------- | -------- | ------ | ------------ | --------------- | 
+            // | Ally    |carnivore | 1 ton  |              | 1               |
+            // | Bob     |herbivore | 3 ton  |              | 2               | 
+            // | Rock    |carnivore | 10 ton |              | 3               | 
+            // | Apple   |carnivore  | 5 ton |              | 4               | 
+            var Ally = new Dinosaurs
+            {
+                Name = "Ally",
+                DietType = "Carnivore",
+                Weight = "1 ton",
+            }
         }
     }
 }
